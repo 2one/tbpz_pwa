7bf865564ad6055c8bf6865bc0866eb9aa024041
RaphaëlD <r.dardeau@gmail.com>
2016-11-12 19:01:21 +0100

    Updates

--- app/config.js
+++ app/config.js
@@ -1,9 +1,9 @@
 var config = {
     viewsPath: "./app/views/",
     incPath: "./app/views/partials/",
-	postsApiUrl: 'http://thebackpackerz.com/wp-json/posts?filter[posts_per_page]=5',
-    categoryPostsApiUrl: 'http://thebackpackerz.com/wp-json/posts?filter[category_name]={slug}&filter[posts_per_page]=5',
-    postApiUrl: 'http://thebackpackerz.com/wp-json/posts?filter[name]={slug}',
-    singlepostApiUrl: 'http://thebackpackerz.com/wp-json/posts/{ID}',
-	navApiUrl: 'http://www.thebackpackerz.com/wp-json/posts/types/posts/taxonomies/category/terms'
+	postsApiUrl: '//thebackpackerz.com/wp-json/posts?filter[posts_per_page]=5',
+    categoryPostsApiUrl: '//thebackpackerz.com/wp-json/posts?filter[category_name]={slug}&filter[posts_per_page]=5',
+    postApiUrl: '//thebackpackerz.com/wp-json/posts?filter[name]={slug}',
+    singlepostApiUrl: '//thebackpackerz.com/wp-json/posts/{ID}',
+	navApiUrl: '//www.thebackpackerz.com/wp-json/posts/types/posts/taxonomies/category/terms'
 };
