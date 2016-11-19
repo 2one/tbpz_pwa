5833a6c7b8d4e8877d3d3eb94e32fe4026563824
IronDev <r.dardeau@gmail.com>
2016-11-19 18:50:47 +0100

    fix misc css

--- src/app/config.js
+++ src/app/config.js
@@ -2,7 +2,7 @@ var config = {
     viewsPath: "./app/views/",
     incPath: "./app/views/partials/",
     apiUrls: {
-        posts: '//thebackpackerz.com/wp-json/posts?type=post&filter[category_name]={slug}&filter[s]={search}&filter[posts_per_page]=4&page={page}',
+        posts: '//thebackpackerz.com/wp-json/posts?type=post&filter[category_name]={slug}&filter[s]={search}&filter[posts_per_page]=5&page={page}',
         post: '//thebackpackerz.com/wp-json/posts?type=post&filter[name]={slug}',
         pages: '//thebackpackerz.com/wp-json/posts?type=page',
         page: '//thebackpackerz.com/wp-json/posts?type=page&filter[name]={slug}',
