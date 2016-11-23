3f9b67c60ac47b7d6110c2ab5f8597b137d03651
RaphaëlD <r.dardeau@gmail.com>
2016-11-23 20:56:00 +0100

    iframe ng-if

--- src/app/config.js
+++ src/app/config.js
@@ -2,11 +2,11 @@ var config = {
     viewsPath: "./app/views/",
     incPath: "./app/views/partials/",
     apiUrls: {
-        posts: '//thebackpackerz.com/wp-json/posts?type=post&filter[category_name]={slug}&filter[s]={search}&filter[posts_per_page]=5&page={page}',
-        post: '//thebackpackerz.com/wp-json/posts?type=post&filter[name]={slug}',
-        pages: '//thebackpackerz.com/wp-json/posts?type=page',
-        page: '//thebackpackerz.com/wp-json/posts?type=page&filter[name]={slug}',
-        singlepost: '//thebackpackerz.com/wp-json/posts/{ID}',
-        nav: '//thebackpackerz.com/wp-json/posts/types/posts/taxonomies/category/terms'
+        posts: "//thebackpackerz.com/wp-json/posts?type=post&filter[category_name]={slug}&filter[s]={search}&filter[posts_per_page]=5&page={page}",
+        post: "//thebackpackerz.com/wp-json/posts?type=post&filter[name]={slug}",
+        pages: "//thebackpackerz.com/wp-json/posts?type=page",
+        page: "//thebackpackerz.com/wp-json/posts?type=page&filter[name]={slug}",
+        singlepost: "//thebackpackerz.com/wp-json/posts/{ID}",
+        nav: "//thebackpackerz.com/wp-json/posts/types/posts/taxonomies/category/terms"
     }
 };
