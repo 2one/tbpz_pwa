0c4092f394d9d4ba600ad510f466948d29046d2b
RaphaëlD <r.dardeau@gmail.com>
2017-07-23 15:41:25 +0200

    'fixes'

--- src/js/angular/filters/parseContent.js
+++ src/js/angular/filters/parseContent.js
@@ -3,6 +3,6 @@ module.exports = function() {
         return String(html)
             .replace(/href="http:\/\/thebackpackerz.com\/([^rewind][^timeline][^\"]+)"/gm, 'href="/$1"')
             .replace(/_blank/gm, '_self')
-            .replace(/src="http:\/\/thebackpackerz.com\/([^\"]+)"/gm, 'src="\/\/secure.thebackpackerz.com\/$1"');;
+            .replace(/src="http:\/\/thebackpackerz.com\/([^\"]+)"/gm, 'src="\/\/secure.thebackpackerz.com\/$1"');
     };
 };
